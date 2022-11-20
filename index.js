ea89429a1e7d4934d20746979bce411bd8f4847d
zhiyu1998 <renzhiyu0416@qq.com>
2022-11-20 16:05:56 +0800

    🎉初始化仓库

---
+++ index.js
@@ -0,0 +1,21 @@
+import fs from 'node:fs'
+import RConfig from './model/index.js'
+
+const versionData = RConfig.getConfig('version')
+
+logger.info('--------------------------')
+logger.info(`RConsole插件${versionData[0].version}初始化~`)
+logger.info('--------------------------')
+
+// 读取功能
+const files = fs
+  .readdirSync('./plugins/RConsole-plugin/apps')
+  .filter((file) => file.endsWith('.js'))
+
+let apps = {}
+for (let file of files) {
+  let name = file.replace('.js', '')
+  apps[name] = (await import(`./apps/${file}`))[name]
+}
+
+export { apps }
