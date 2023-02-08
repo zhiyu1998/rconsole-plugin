b9d82d41348eff400c9e615c003c784e32b41c6c
zhiyu1998 <renzhiyu0416@qq.com>
2023-02-08 12:54:17 +0800

    🐞 fix: 设置部分功能为可选

--- apps/mystery.js
+++ apps/mystery.js
@@ -47,7 +47,7 @@ export class mystery extends plugin {
                 },
                 {
                     reg: '^#(啊?|啊？)$', fnc: 'aaa'
-                },
+                }
                 // {
                 //     reg: '^#我靠', fnc: 'tuiimg'
                 // }
