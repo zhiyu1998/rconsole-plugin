3251026d00dbf0977abf4cbf6054ea50200bc7ca
zhiyu1998 <renzhiyu0416@qq.com>
2023-02-15 12:12:32 +0800

    📃 docs: 更新README

--- test/main.py
+++ test/main.py
@@ -6,7 +6,7 @@ import aiohttp
 import motor
 from bs4 import BeautifulSoup
 
-url = 'https://www.tuiimg.com/meinv/'
+url = ''
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35",
