6878ab96016ec8ac4b9d2b1c91884b495bc54e1b
RrOrange <1370346834@qq.com>
2023-03-27 19:42:58 +0800

    🌈 style: 整理文件

--- utils/bilibili.js
+++ utils/bilibili.js
@@ -95,32 +95,4 @@ async function mergeFileToMp4 (vFullFileName, aFullFileName, outputFileName, sho
     }
 }
 
-async function getDynamic(dynamicId) {
-    const dynamicApi = `https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/get_dynamic_detail?dynamic_id=${dynamicId}`
-    return axios.get(dynamicApi, {
-        headers: {
-            'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
-            'referer': 'https://www.bilibili.com',
-        }
-    }).then(resp => {
-        const dynamicData = resp.data.data.card
-        const card = JSON.parse(dynamicData.card)
-        const dynamicOrigin = card.item
-        const dynamicDesc = dynamicOrigin.description
-
-        const pictures = dynamicOrigin.pictures
-        let dynamicSrc = []
-        for (let pic of pictures) {
-            const img_src = pic.img_src
-            dynamicSrc.push(img_src)
-        }
-        // console.log(dynamic_src)
-        return {
-            dynamicSrc,
-            dynamicDesc
-        }
-    })
-}
-
-export { downloadBFile, getDownloadUrl, mergeFileToMp4, getDynamic }
+export { downloadBFile, getDownloadUrl, mergeFileToMp4 }
