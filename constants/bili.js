061002f24d4302d92ed8b901b80b6546d307a06c
zhiyu <542716863@qq.com>
2024-01-26 12:49:16 +0800

    ✨ feat: 新增哔哩哔哩官方AI总结、哔哩哔哩音乐提取
    1. 新增哔哩哔哩音乐提取，使用“bili音乐+链接”即可提取视频中的音乐
    2. 重构部分代码
    3. 更换tiktok API
    4. 更换视频总结方式GPT为官方的视频摘要（免费使用）
    5. 删除GPT相关内容

---
+++ constants/bili.js
@@ -0,0 +1,20 @@
+/**
+ * AI总结API
+ * https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/misc/sign/wbi.md
+ * @type {string}
+ */
+export const BILI_SUMMARY = "https://api.bilibili.com/x/web-interface/view/conclusion/get"
+
+/**
+ * 视频基本信息API
+ * https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/video/info.md
+ * @type {string}
+ */
+export const BILI_VIDEO_INFO = "http://api.bilibili.com/x/web-interface/view"
+
+/**
+ * 登录基本信息
+ * https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/login/login_info.md#%E5%AF%BC%E8%88%AA%E6%A0%8F%E7%94%A8%E6%88%B7%E4%BF%A1%E6%81%AF
+ * @type {string}
+ */
+export const BILI_NAV = "https://api.bilibili.com/x/web-interface/nav"
