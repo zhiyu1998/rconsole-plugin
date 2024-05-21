6753bba89cc2365ae546f6e88e6135fbbba26aa0
zhiyu1998 <542716863@qq.com>
2024-05-21 13:26:35 +0800

    ✨ feat: 添加哔哩哔哩BV直接跳转

--- apps/tools.js
+++ apps/tools.js
@@ -113,7 +113,7 @@ export class tools extends plugin {
                     permission: 'master',
                 },
                 {
-                    reg: "(bilibili.com|b23.tv|t.bilibili.com)",
+                    reg: "(bilibili.com|b23.tv|t.bilibili.com|^BV[1-9a-zA-Z]{10}$)",
                     fnc: "bili",
                 },
                 {
@@ -386,6 +386,11 @@ export class tools extends plugin {
         const urlRex = /(?:https?:\/\/)?www\.bilibili\.com\/[A-Za-z\d._?%&+\-=\/#]*/g;
         const bShortRex = /(http:|https:)\/\/b23.tv\/[A-Za-z\d._?%&+\-=\/#]*/g;
         let url = e.msg === undefined ? e.message.shift().data.replaceAll("\\", "") : e.msg.trim().replaceAll("\\", "");
+        // 直接发送BV号的处理
+        if (/^BV[1-9a-zA-Z]{10}$/.exec(url)?.[0]) {
+            url = `https://www.bilibili.com/video/${ url }`;
+            logger.info(url)
+        }
         // 短号处理
         if (url.includes("b23.tv")) {
             const bShortUrl = bShortRex.exec(url)?.[0];
