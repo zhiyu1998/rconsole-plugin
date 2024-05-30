f91d3b495c6a46b64c8dc4dbc664e07e4ffe6807
zhiyu1998 <542716863@qq.com>
2024-05-30 22:12:19 +0800

    🐞 fix: 修复netease无法解析歌曲问题

--- apps/tools.js
+++ apps/tools.js
@@ -1029,12 +1029,12 @@ export class tools extends plugin {
             // 动态判断后缀名
             const extensionPattern = /\.([a-zA-Z0-9]+)$/;
             const musicExt = url.match(extensionPattern)?.[0].replace("\.", "");
-            logger.info(musicExt);
             // 下载音乐
             downloadAudio(url, this.getCurDownloadPath(e), title, 'follow', musicExt).then(async path => {
                 // 发送语音
                 // e.reply(segment.record(path));
                 // 判断是不是icqq
+                await e.reply(segment.record(path));
                 await this.uploadGroupFile(e, path);
                 await checkAndRemoveFile(path);
             }).catch(err => {
