10ff56a6e9c6a3607c91b0c549fb3b882902a62c
zhiyu1998 <542716863@qq.com>
2024-08-17 10:52:29 +0800

    🐞 fix: 修复评论路径

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -2,13 +2,11 @@
   <div class="comments">
     <!-- params generate in https://giscus.app/zh-CN -->
     <Giscus
-        v-if="showComment"
         repo="zhiyu1998/rconsole-plugin"
         repo-id="R_kgDOLNdlcQ"
         category="Q&A"
         category-id="DIC_kwDOLNdlcc4ChqqQ"
-        mapping="specific"
-        term="pathname"
+        mapping="pathname"
         strict="1"
         reactions-enabled="1"
         emit-metadata="0"
@@ -21,7 +19,6 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { ref, watch, nextTick, computed } from "vue";
 import { useData, useRoute } from "vitepress";
 import Giscus from "@giscus/vue";
 
@@ -30,20 +27,6 @@ const { isDark } = useData();
 
 const theme = computed(() => (isDark.value ? "dark_dimmed" : "light_high_contrast"));
 
-// language变化不会触发重新加载，这里v-if强制刷新
-const showComment = ref(true);
-watch(
-    () => route.path,
-    () => {
-      showComment.value = false;
-      nextTick(() => {
-        showComment.value = true;
-      });
-    },
-    {
-      immediate: true,
-    }
-);
 </script>
 <style scoped>
 .comments {
