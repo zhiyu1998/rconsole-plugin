b4a0593a6c8b7eeed739b0ff08ff75e648ad2ec7
zhiyu1998 <542716863@qq.com>
2024-08-22 12:08:25 +0800

    🦄 reactor: 重构`tieba`解析

--- apps/tools.js
+++ apps/tools.js
@@ -1906,7 +1906,7 @@ export class tools extends plugin {
         const top = postList[0];
         const { title, content } = top;
         let sendContent = `${this.identifyPrefix}识别：贴吧，${ title }`
-        if (content?.[0]?.cdn_src !== undefined) {
+        if (content.length > 0) {
             sendContent = [sendContent]
             for (let contentElement of content) {
                 if (contentElement?.cdn_src !== undefined) {
