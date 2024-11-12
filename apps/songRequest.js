bfde563b57d2f028e8dd3e620e0fec5123381297
秋刀鱼 <9903082+qdyovo@user.noreply.gitee.com>
2024-11-12 14:24:06 +0800

    🦄 reactor: 避免误触发

--- apps/songRequest.js
+++ apps/songRequest.js
@@ -33,7 +33,7 @@ export class songRequest extends plugin {
                     fnc: "upLoad"
                 },
                 {
-                    reg: '^#?我的云盘$|#?rnc|#?RNC',
+                    reg: '^#?我的云盘$|#rnc|#RNC',
                     fnc: 'myCloud',
                     permission: 'master'
                 },
@@ -43,7 +43,7 @@ export class songRequest extends plugin {
                     permission: 'master'
                 },
                 {
-                    reg: '^#?上传云盘|#?上传网盘$|#?rnu|#?RNU',
+                    reg: '^#?上传云盘|#?上传网盘$|#rnu|#RNU',
                     fnc: 'uploadCloud',
                     permission: 'master'
                 },
