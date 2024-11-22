f838a2a90f5df2d84cd9e071966731bc597be6ff
zhiyu1998 <542716863@qq.com>
2024-11-22 22:14:46 +0800

    ✨feat: 添加更新信息

--- server/components/home/bot-config.jsx
+++ server/components/home/bot-config.jsx
@@ -1,32 +1,46 @@
+import { useEffect, useState } from "react";
+import { GIT_COMMIT_URL, GIT_VERSION_URL } from "../../constants/api.js";
+
 export function BotConfig() {
+
+    const [version, setVersion] = useState("v0.0.0");
+
+    const [commit, setCommit] = useState(null);
+
+    useEffect(() => {
+        fetch(GIT_VERSION_URL).then(response => response.json()).then(data => setVersion(data.name));
+        fetch(GIT_COMMIT_URL).then(response => response.json()).then(data => setCommit(data));
+    }, []);
+
     return (
         <div className="card bg-base-100 shadow-xl">
             <div className="card-body">
-                <h2 className="card-title text-lg font-bold">🔥快捷更新</h2>
+                <h2 className="card-title text-lg font-bold">🔥更新看板</h2>
                 <div className="grid grid-cols-1 gap-2">
                     <div className="flex justify-between items-center">
                         <div>
-                            <h3 className="font-bold">检查更新</h3>
-                            <p>当前最新版本为：v0.0.0</p>
+                            <h3 className="font-bold">最新版本</h3>
+                            <p>当前最新版本为：{ version }</p>
+                        </div>
+                        <button className="btn btn-ghost"
+                                onClick={ () => fetch(GIT_VERSION_URL).then(response => response.json()).then(data => setVersion(data.name)) }>检查更新
+                        </button>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <h3 className="font-bold">手动更新</h3>
+                            <p>R 插件的自动选择更新 / 强制更新</p>
                         </div>
                         <button className="btn btn-warning">🚧施工</button>
                     </div>
                     <div className="flex justify-between items-center">
                         <div>
-                            <h3 className="font-bold">非强制更新</h3>
-                            <p>R 插件的非强制更新，力度小</p>
+                            <h3 className="font-bold">最近更新</h3>
+                            <span><a href={ commit?.url }>[{ commit?.author }]{ commit?.message }</a></span>
                         </div>
-                        <button className="btn btn-warning">🚧施工</button>
-                    </div>
-                    <div className="flex justify-between items-center">
-                        <div>
-                            <h3 className="font-bold">强制更新</h3>
-                            <p>R 插件的强制更新，力度大</p>
-                        </div>
-                        <button className="btn btn-warning">🚧施工</button>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- server/constants/api.js
+++ server/constants/api.js
@@ -5,3 +5,7 @@ export const SYSTEM_BASE_URL = `${BASE_URL}/system`;
 export const NETWORK_BASE_URL = `${BASE_URL}/network?url=`;
 
 export const BOT_INFO_URL = `${ BASE_URL }/bot`;
+
+export const GIT_VERSION_URL = `${ BASE_URL }/version`;
+
+export const GIT_COMMIT_URL = `${ BASE_URL }/commit`;
